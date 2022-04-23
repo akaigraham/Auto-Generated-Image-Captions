ef09d4ec604803ae2602020bb0a09782a5e906cf
Addington Graham <akaigraham@gmail.com>
2022-04-23 18:14:35 -0400

    predicting captions on unseen images

--- model.py
+++ model.py
@@ -1,9 +1,25 @@
-# import dependencies
-from array import array
+# import necessary libraries
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup 
+from PIL import Image
+from nltk import word_tokenize
+import re
+
+# transfer learning model
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
 from keras.models import Model
 from keras.preprocessing import sequence, image 
+
+# progress bars
+from alive_progress import alive_bar; import time
+
+# serialization
+import pickle 
+
+# deep learning model
 from keras import Input, layers, optimizers
 from keras.layers import LSTM, Embedding, Dense, Activation, Flatten, Reshape, Dropout
 from keras.layers.merge import add
@@ -12,9 +28,15 @@ from tensorflow.keras.utils import to_categorical
 from keras.models import load_model
 
 
+# instantiate V3 model and set weights
+transfer_model = InceptionV3(weights='imagenet')
+
+# remove softmax layer as not needed for image classification  
+new_transfer_model = Model(transfer_model.input, transfer_model.layers[-2].output)
+
 # preprocess image for transfer learning
 # with InceptionV3
-def preprocess(image_path, resize=(350,350)):
+def resize(image_path):
     """
     Preprocesses image using InceptionV3. 
     Input: 
@@ -25,14 +47,15 @@ def preprocess(image_path, resize=(350,350)):
     """
 
     # load and convert image to numpy array
-    img_pil = image.load_img(image_path, target_size=resize)
-    img_numpy = image.img_to_array(img_pil)
+    img_pil = image.load_img(image_path, target_size=(299,299))
+    X = image.img_to_array(img_pil)
 
     # expand shape of array
-    img_expanded = np.expand_dims(img_numpy, axis=0)
+    X = np.expand_dims(X, axis=0)
 
     # preprocess for InceptionV3 and return
-    return preprocess_input(img_expanded)
+    return preprocess_input(X)
+
 
 # encode preprocessed image using InceptionV3
 def encode(image_path):
@@ -45,13 +68,6 @@ def encode(image_path):
         [...]
     """
 
-    # instantiate V3 model and set weights
-    transfer_model = InceptionV3(weights='imagenet')
-
-    # remove softmax layer as not needed for image classification
-    new_transfer_model = Model(transfer_model.input, 
-                               transfer_model.layers[-2].output)
-
     # preprocess and generate predictions
     img_preprocessed = resize(image_path)
     img_preds = new_transfer_model.predict(img_preprocessed)
@@ -60,8 +76,8 @@ def encode(image_path):
     return np.reshape(img_preds, img_preds.shape[1])
 
 # function to set up model
-def generate_caption(image_path, in_text='startseq', max_length=80,
-                     word_to_idx, idx_to_word, model):
+def generate_caption(encoded_image, word_to_idx, idx_to_word, 
+                     model, in_text='startseq', max_length=80):
     """
     Use greedy search and cloud_trained model to generate captions.
     Input:
@@ -85,7 +101,7 @@ def generate_caption(image_path, in_text='startseq', max_length=80,
 
         # use model to generate predictions 
         # get highest probability prediction for next word
-        y_hat = model.predict([image_path, sequence], verbose=0)
+        y_hat = model.predict([encoded_image, sequence], verbose=0)
         y_hat = np.argmax(y_hat)
 
         # convert model prediction back to string
@@ -101,8 +117,26 @@ def generate_caption(image_path, in_text='startseq', max_length=80,
     caption_tokens = in_text.split() # split in_text by word
     caption_tokens = caption_tokens[1:-1] # ignore startseq and endseq
     return ' '.join(caption_tokens) # return string caption
-
-
-
-# Load model
+    
+# Load model and necessary objects
 trained_model = load_model('cloud_trained_model.h5')
+
+# load word_to_idx
+with open('cloud_files/word_to_idx.pickle', 'rb') as f:
+    word_to_idx = pickle.load(f)
+
+# load idx_to_word
+with open('cloud_files/idx_to_word.pickle', 'rb') as f:
+    idx_to_word = pickle.load(f)
+
+# test on image not in test set
+test_img_path = '/Users/addingtongraham/Desktop/kg.jpg'
+
+# encode img
+test_img_encoded = encode(test_img_path).reshape((1,2048))
+
+# generate captions
+caption = generate_caption(test_img_encoded, word_to_idx=word_to_idx, idx_to_word=idx_to_word, model=trained_model)
+
+print('test')
+print(f'Generated Caption:\n{caption}')
